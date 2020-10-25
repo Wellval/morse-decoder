686de0d203d52e55cc37ef0a78f0c18be900b2b0
Valentina Masalkova <vrassone@gmail.com>
2020-10-25 22:12:21 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+String.prototype.replaceAll = function (a, b) {
+    let result = this;
+    while(result.indexOf(a) !== -1) {
+        result = result.replace(a, b);
+    }
+    return result;
+}
+
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        let bin = expr.substr(i, 10);
+        if (bin[0] === '*') {
+            result += " ";
+        } else {
+            let counter = 0;
+            for (let j = 0; j < bin.length; ++j) {
+                if (bin[j] === '0') {
+                    counter++;
+                } else {
+                    break;
+                }
+            }
+            bin = bin.substr(counter, bin.length - counter).replaceAll("11", "-").replaceAll("10", ".");
+            result += MORSE_TABLE[bin];
+        }
+    }
+    return result;
 }
 
 module.exports = {
